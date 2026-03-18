7abf0fb3b74bdcb818a7a3c76df0c3418de3281b
Kundan Gosavi <gosavikundan4@gmail.com>
2026-03-18 17:49:17 +0530

    Added Hero Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,14 @@ export default {
     extend: {
         fontFamily: {
         sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        serif: ['Cormorant Garamond', 'TASA Orbiter', 'ui-serif', 'serif'],
+        display: ['Anton', 'TASA Orbiter', 'Inter', 'Montserrat'],
+        poppins: ['Poppins', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+        anton: ['Anton', 'sans-serif'],
+        cormorant: ['Cormorant Garamond', 'serif'],
+        orbiter: ['TASA Orbiter', 'serif'],
+        playfair: ['Playfair Display', 'serif'],
       },
     },
   },
